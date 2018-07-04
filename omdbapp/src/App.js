0498be60c091e5bfb38c8dd268158ab6bd5be23b
dixitomkar1809 <omedxt@gmail.com>
2018-07-04 12:54:26 -0500

    Navbar Form Working

--- omdbapp/src/App.js
+++ omdbapp/src/App.js
@@ -3,14 +3,40 @@ import './App.css';
 
 
 class App extends Component {
-  
+  constructor(props){
+    super(props);
+    this.state = {searchInput:'asd'};
+    console.log("From the constructor");
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({
+      searchInput: event.target.value
+    });
+    console.log(event);
+  }
+
+  handleSubmit(event){
+    console.log(event);
+  }
+
   render() {
     return (
-      <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">MovieDB</h1>
-        <p class="lead">Information about any movie released in any year with its IMDB rating, to help you choose the right one to watch !</p>
-      </div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">MovieDB</h1>
+          <p className="lead">Information about any movie released in any year with its IMDB rating, to help you choose the right one to watch !</p>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Search..." value={this.state.searchInput} onChange={this.handleChange}/>
+            </div>
+            <div className="form-group">
+              <input className="form-control" type="Submit" placeholder="Search" />
+            </div>
+        </form>
+        </div>
     </div>
     );
   }
