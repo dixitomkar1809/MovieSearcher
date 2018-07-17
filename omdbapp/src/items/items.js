a3616161411581b71f6957a1507ad71bc3c6eb4b
dixitomkar1809 <omedxt@gmail.com>
2018-07-16 19:41:16 -0500

    Need to take care in case of tablets phone and desktop is ifne

--- omdbapp/src/items/items.js
+++ omdbapp/src/items/items.js
@@ -12,21 +12,19 @@ class items extends Component {
                     <img className="card-img img-fluid" src={"https://image.tmdb.org/t/p/original"+this.props.backdrop_path} alt={this.props.title} />
                     <div className="card-img-overlay  black-background">
                         <div className="row margin-top-adjust">
-                            {/* <div className="col-md-3 col-sm-3 col-3">
-                                <img className="img-thumbnail" src={"https://image.tmdb.org/t/p/original"+this.props.poster} alt={this.props.title} />
-                            </div> */}
                             <div className="col-md-12 col-sm-12 col-12">
                                 <h1 className="card-title display-3">{this.props.title}</h1>
                                 <div className="row">
-                                    <div className="col-md-6 col-sm-3 col-6">
-                                        <h4 className="margin-top-adjust">Release Date: {this.props.release_date}</h4>
+                                    <div className="col-md-3 col-sm-3 col-3">
+                                        <img src={"https://image.tmdb.org/t/p/original"+this.props.poster} className="img-thumbnail" alt={this.props.title} />
                                     </div>
-                                    <div className="col-md-6 col-sm-3 col-6">
-                                        <h4 className="margin-top-adjust">Rating: {this.props.rating}/10 </h4>
+                                    <div className="col-md-9 col-sm-9 col-9">
+                                        <h4>Release Date: {this.props.release_date}</h4>
+                                        <h4>Rating: {this.props.rating}/10 </h4>
+                                        <h4>Genre: &nbsp; {this.props.genre.map((item, index) => <Genre key={index} genre={item} /> )}</h4>
+                                        <h4 className="text-justify scrollable">{this.props.overview}</h4>
                                     </div>
                                 </div>
-                                <h4 className="margin-top-adjust">Genre: &nbsp; {this.props.genre.map((item, index) => <Genre key={index} genre={item} /> )}</h4>
-                                <h4 className="text-justify margin-top-adjust">{this.props.overview}</h4>
                             </div>
                         </div>
                     </div>
